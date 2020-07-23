a5782a7dba844b29e8edd6b737bf36ddf2ab9c33
Tatang Romadhona <itsme@tatang.dev>
2020-07-24 06:11:34 +0700

    [tatangdev] integrate user-delete_API

--- routes/users.js
+++ routes/users.js
@@ -7,5 +7,7 @@ const verifyToken = require('../middlewares/verifyToken');
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
 router.put('/', verifyToken, usersHandler.update);
+router.get('/', verifyToken, usersHandler.getUser);
+router.post('/logout', verifyToken, usersHandler.logout);
 
 module.exports = router;
