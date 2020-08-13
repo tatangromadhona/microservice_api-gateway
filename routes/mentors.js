25cad49da6709fc0bd3e6ea23a5ffc01cd1b892d
Tatang Romadhona <itsme@tatang.dev>
2020-08-13 15:42:56 +0700

    [tatangdev] integrate service_course/API Lessons

--- routes/mentors.js
+++ routes/mentors.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const mentorsHandler = require('./handler/mentors');
 
-router.post('/',mentorsHandler.create);
 router.get('/',mentorsHandler.getAll);
 router.get('/:id',mentorsHandler.get);
+
+router.post('/',mentorsHandler.create);
 router.put('/:id',mentorsHandler.update);
 router.delete('/:id',mentorsHandler.destroy);
 
